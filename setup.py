1fe682c841541994ff959ade6767b3e6261aa597
Daniel Brandt <me@dbrandt.se>
2015-01-05 11:14:57 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+kwargs = {
+    "name": "pec",
+    "version": "0.1-prerelease",
+    "description": "Interface to the process event connector in linux kernels.",
+    "author": "Daniel Brandt",
+    "author_email": "me@dbrandt.se",
+    "url": "http://dbrandt.se/pec",
+    "scripts": ["bin/pec_listener.py"],
+    "packages": find_packages(),
+}
+
+if __name__ == "__main__":
+    setup(**kwargs)
